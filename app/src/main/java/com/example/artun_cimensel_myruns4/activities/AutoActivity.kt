a2e901e1fb11cfd5be8ee02156d3c742f41c09cd
ACimensel <cimensel.a@hotmail.com>
2022-11-25 15:15:55 -0800

    removed comments

--- app/src/main/java/com/example/artun_cimensel_myruns4/activities/AutoActivity.kt
+++ app/src/main/java/com/example/artun_cimensel_myruns4/activities/AutoActivity.kt
@@ -30,25 +30,23 @@ class AutoActivity : AppCompatActivity() {
 
     private val classifierBroadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
-            val classifier = intent.getDoubleExtra(Globals.KEY_CLASS_RESULT, -1.0)
-//            historyEntry.updateByInference(classifier)
-//            historyEntry.activityType = blah
+            when (intent.getDoubleExtra(Globals.KEY_CLASS_RESULT, -1.0)) {
+                0.0 -> {
+                    titleLabel.text = "You are standing."
+                    historyEntry.activityType = 2
+                }
+                1.0 -> {
+                    titleLabel.text = "You are walking."
+                    historyEntry.activityType = 1
+                }
+                2.0 -> {
+                    titleLabel.text = "You are running."
+                    historyEntry.activityType = 0
+                }
+                else -> {
+                    titleLabel.text = "Unknown."
 
-            if(classifier == 0.0){
-                titleLabel.text = "You are standing."
-                historyEntry.activityType = 2
-            }
-            else if(classifier == 1.0){
-                titleLabel.text = "You are walking."
-                historyEntry.activityType = 1
-            }
-            else if(classifier == 2.0){
-                titleLabel.text = "You are running."
-                historyEntry.activityType = 0
-            }
-            else{
-                titleLabel.text = "Unknown."
-
+                }
             }
         }
     }
@@ -69,7 +67,6 @@ class AutoActivity : AppCompatActivity() {
         mClassifierIntentFilter = IntentFilter()
         mClassifierIntentFilter.addAction(Globals.ACTION_MOTION_UPDATED)
 
-        // New code here
         mServiceIntent = Intent(this, SensorService::class.java)
         startService(mServiceIntent)
     }
@@ -98,33 +95,4 @@ class AutoActivity : AppCompatActivity() {
         finish()
         super.onDestroy()
     }
-
-//    class ClassifierBroadcastReceiver(handler: Handler) : BroadcastReceiver() {
-//        private val handler : Handler // Handler used to execute code on the UI thread
-//
-//        init {
-//            this.handler = handler
-//        }
-//
-//        override fun onReceive(context: Context?, intent: Intent?) {
-//            val classifier = intent?.getDoubleExtra(Globals.KEY_CLASS_RESULT, -1.0)
-//
-//
-//            // Post the UI updating code to our Handler
-//            handler.post(object : Runnable {
-//                override fun run() {
-//
-////              Log.d("DEBUG:", "____________________onReceive $classifier")
-//                    Toast.makeText(context, classifier.toString(), Toast.LENGTH_SHORT).show() // TODO remove
-//
-////                if(classifier == 0.0)
-////                    titleLabel.text = "You are idle."
-//
-////                    handler.postDelayed(this, 1000)
-//                }
-//            })
-//
-//            }
-//        }
-
 }
